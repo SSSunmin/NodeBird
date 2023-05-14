67df9ba125298e60492f3f5b770828f12a0f5b9b
sunmin <dlatjsals123@naver.com>
2023-05-14 20:28:45 +0900

    feat: 유저 프로필 페이지 추가

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,15 +1,30 @@
 import React from "react";
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 import Head from "next/head";
 
 const Profile = () => {
+  const followingList = [
+    { nickname: "선녀니" },
+    { nickname: "복치" },
+    { nickname: "기요미" },
+  ];
+  const followerList = [
+    { nickname: "선녀니" },
+    { nickname: "복치" },
+    { nickname: "기요미" },
+  ];
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
       <AppLayout>
-        <div>내 프로필</div>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
       </AppLayout>
     </>
   );
