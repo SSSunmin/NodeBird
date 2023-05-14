978aa03a38df8030adaa66baafeaea1a00a76453
sunmin <dlatjsals123@naver.com>
2023-05-14 20:33:10 +0900

    feat: 로그인 했을 때 홈화면 유저 프로필 추가

---
+++ front/components/UserProfile.js
@@ -0,0 +1,33 @@
+import React, { useCallback } from "react";
+import PropTypes from "prop-types";
+import { Avatar, Button, Card } from "antd";
+
+const UserProfile = ({ setIsLoggedIn }) => {
+  const onLogout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+  return (
+    <Card
+      actions={[
+        <div key="twit">
+          짹짹 <br />0
+        </div>,
+        <div key="followings">
+          팔로잉 <br />0
+        </div>,
+        <div key="followers">
+          팔로워 <br />0
+        </div>,
+      ]}
+    >
+      <Card.Meta avatar={<Avatar>SUN</Avatar>} title="sunmin" />
+      <Button onClick={onLogout}>로그아웃</Button>
+    </Card>
+  );
+};
+
+UserProfile.propTypes = {
+  setIsLoggedIn: PropTypes.func.isRequired,
+};
+
+export default UserProfile;
